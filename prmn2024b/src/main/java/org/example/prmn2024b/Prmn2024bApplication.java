5774f1f0fd5a0b1a1c6551abba87d9f86f665f0f
yamahira0126 <b2222490@photon.chitose.ac.jp>
2024-10-24 22:57:01 +0900

    lecture01の追加

---
+++ prmn2024b/src/main/java/org/example/prmn2024b/Prmn2024bApplication.java
@@ -0,0 +1,13 @@
+package org.example.prmn2024b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prmn2024bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prmn2024bApplication.class, args);
+    }
+
+}
---
+++ prmn2024b/src/test/java/org/example/prmn2024b/Prmn2024bApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prmn2024b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prmn2024bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
